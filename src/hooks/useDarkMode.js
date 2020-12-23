705ef9d163e55f4585adc52fab389259c09eb3a5
Caroline <carolinefallonlucas@gmail.com>
2020-12-22 20:43:22 -0500

    mvp final

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import useLocalStorage from './useLocalStorage'
+
+export const useDarkMode = (key) =>
+{
+    const [value, setValue] = useLocalStorage(key, false)
+  const toggleDarkMode = () => {setValue(!value)}
+
+
+return [value, toggleDarkMode]
+}
+
